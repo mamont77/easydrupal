0c01ec75bd251b1efeae0ad1a52447ebbb413aea
Ruslan Piskarev <ruslan.piskarev@gmail.com>
2019-01-17 21:12:50 +0200

    Updated simple_sitemap module.

--- modules/contrib/simple_sitemap/src/Annotation/SitemapGenerator.php
+++ modules/contrib/simple_sitemap/src/Annotation/SitemapGenerator.php
@@ -5,16 +5,16 @@ namespace Drupal\simple_sitemap\Annotation;
 use Drupal\Component\Annotation\Plugin;
 
 /**
- * Defines a UrlGenerator item annotation object.
+ * Defines a SitemapGenerator item annotation object.
  *
  * @package Drupal\simple_sitemap\Annotation
  *
- * @see \Drupal\simple_sitemap\Plugin\simple_sitemap\UrlGenerator\UrlGeneratorManager
+ * @see \Drupal\simple_sitemap\Plugin\simple_sitemap\SitemapGenerator\SitemapGeneratorManager
  * @see plugin_api
  *
  * @Annotation
  */
-class UrlGenerator extends Plugin {
+class SitemapGenerator extends Plugin {
 
   /**
    * The generator ID.
