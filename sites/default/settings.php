eca7a6b3603b6dd2c4d58360ea1bd652d4a5bc5c
Ruslan Piskarev <ruslan.piskarev@gmail.com>
2021-10-21 15:47:55 +0300

    Add redis settings.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -647,6 +647,9 @@ if ($settings['hash_salt']) {
 # $config['system.performance']['fast_404']['paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
 # $config['system.performance']['fast_404']['html'] = '<!DOCTYPE html><html><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
 
+$settings['redis.connection']['interface'] = 'PhpRedis'; // Can be "Predis".
+$settings['cache']['default'] = 'cache.backend.redis';
+
 /**
  * Load services definition file.
  */
