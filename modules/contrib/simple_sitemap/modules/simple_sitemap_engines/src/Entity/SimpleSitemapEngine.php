c36ca6dc9a3eec40652528349fa3ed6ec11433ca
Ruslan Piskarev <ruslan.piskarev@gmail.com>
2021-12-12 20:30:57 +0200

    - Upgrading drupal/devel (4.1.1 => 4.1.2)   - Upgrading drupal/simple_sitemap (3.11.0 => 4.0.0)   - Upgrading drupal/token (1.9.0 => 1.10.0)

--- modules/contrib/simple_sitemap/modules/simple_sitemap_engines/src/Entity/SimpleSitemapEngine.php
+++ modules/contrib/simple_sitemap/modules/simple_sitemap_engines/src/Entity/SimpleSitemapEngine.php
@@ -29,7 +29,7 @@ use Drupal\Core\Config\Entity\ConfigEntityBase;
  *   }
  * )
  */
-class SearchEngine extends ConfigEntityBase {
+class SimpleSitemapEngine extends ConfigEntityBase {
 
   /**
    * The search engine ID.
@@ -56,7 +56,7 @@ class SearchEngine extends ConfigEntityBase {
   public $url;
 
   /**
-   * List of sitemap variants to be submitted to this search engine.
+   * List of sitemaps to be submitted to this search engine.
    *
    * @var array
    */
@@ -69,7 +69,7 @@ class SearchEngine extends ConfigEntityBase {
    *   The search engine label.
    */
   public function __toString() {
-    return $this->label();
+    return (string) $this->label();
   }
 
 }
