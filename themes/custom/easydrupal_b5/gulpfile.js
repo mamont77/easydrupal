43b8a1f2b57d255eeca52b7c6004a30d57b26fcb
Ruslan Piskarev <ruslan.piskarev@gmail.com>
2024-02-14 13:15:27 +0200

    Update gulpfile.js.

--- themes/custom/easydrupal_b5/gulpfile.js
+++ themes/custom/easydrupal_b5/gulpfile.js
@@ -1,52 +1,72 @@
 'use strict';
 
-const publicPath = "./dist";
-const resourcesPath = "./assets";
-
-var gulp = require('gulp'),
-  imagemin = require('gulp-imagemin'),
-  spritesmith = require('gulp.spritesmith'),
-  sass = require('gulp-sass'),
+const gulp = require('gulp'),
+  sass = require('gulp-sass')(require('sass')),
   sourcemaps = require('gulp-sourcemaps'),
   postcss = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer'),
-  mqpacker = require('css-mqpacker');
+  autoprefixer = require('autoprefixer');
 
-gulp.task('sprite', async function () {
-  var spriteData = gulp.src(resourcesPath + 'images/sprite-src/*.png').pipe(spritesmith({
-    imgName: 'sprite.png',
-    imgPath: '../images/sprite.png',
-    cssName: '_sprite.scss',
-    padding: 2
-  }));
-  spriteData.img.pipe(gulp.dest('images'));
-  spriteData.css.pipe(gulp.dest('scss'));
-});
+const publicPath = './dist',
+  resourcesPath = './assets';
 
-gulp.task('imagemin', async function () {
-  return gulp.src(resourcesPath + '/images/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest(publicPath + '/images'))
-});
-
-gulp.task('sass', async function () {
-  return gulp.src([resourcesPath + '/scss/*.scss'])
+async function compileSass() {
+  return gulp.src([
+    resourcesPath + '/scss/*.scss',
+  ])
     .pipe(sourcemaps.init())
     .pipe(sass({
       outputStyle: 'expanded',
-      precision: 10
+      precision: 10,
     }).on('error', sass.logError))
     .pipe(postcss([
-      mqpacker({
-        sort: true
-      }),
       autoprefixer({
-        overrideBrowserslist: ['last 2 versions']
-      })]))
+        overrideBrowserslist: ['last 2 versions'],
+        grid: true,
+        remove: false,
+      }),
+    ]))
     .pipe(sourcemaps.write('../sourcemaps'))
     .pipe(gulp.dest(publicPath + '/css'));
-});
+}
 
-gulp.task('watch', function () {
-  gulp.watch(['images/sprite-src/*.png', resourcesPath + '/scss/**/*.scss'], ['sprite', 'imagemin', 'sass']);
-});
+async function deploy() {
+  return gulp.src([
+    resourcesPath + '/scss/*.scss',
+  ])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'expanded',
+      precision: 10,
+    }).on('error', sass.logError))
+    .pipe(postcss([
+      autoprefixer({
+        overrideBrowserslist: ['last 2 versions'],
+        grid: true,
+        remove: false,
+      }),
+    ]))
+    .pipe(sourcemaps.write('../sourcemaps'))
+    .pipe(gulp.dest(publicPath + '/css'));
+}
+
+async function imageminTask() {
+  const imagemin = (await import('gulp-imagemin')).default;
+  return gulp.src(resourcesPath + 'images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest(publicPath + '/images'));
+}
+
+async function watch() {
+  gulp.watch([
+    resourcesPath + '/scss/*.scss',
+  ], compileSass);
+  gulp.watch([resourcesPath + 'images/**'], imageminTask);
+}
+
+// Define tasks
+gulp.task('sass', compileSass);
+gulp.task('deploy', deploy);
+gulp.task('imagemin', imageminTask);
+gulp.task('watch', watch);
+
+exports.default = watch;
