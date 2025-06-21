d49b62e40d285d818090c8e7c7c128edd31909af
Ruslan Piskarev <ruslan.piskarev@gmail.com>
2025-06-21 11:54:43 +0300

    Add mysql5 module.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -626,6 +626,34 @@ if ($settings['hash_salt']) {
 # $config['user.settings']['anonymous'] = 'Visitor';
 
 /**
+ * Exclude modules from configuration synchronization.
+ *
+ * On config export sync, no config or dependent config of any excluded module
+ * is exported. On config import sync, any config of any installed excluded
+ * module is ignored. In the exported configuration, it will be as if the
+ * excluded module had never been installed. When syncing configuration, if an
+ * excluded module is already installed, it will not be uninstalled by the
+ * configuration synchronization, and dependent configuration will remain
+ * intact. This affects only configuration synchronization; single import and
+ * export of configuration are not affected.
+ *
+ * Drupal does not validate or sanity check the list of excluded modules. For
+ * instance, it is your own responsibility to never exclude required modules
+ * because it would mean that the exported configuration cannot be imported
+ * anymore.
+ *
+ * This is an advanced feature and using it means opting out of some of the
+ * guarantees the configuration synchronization provides. It is not recommended
+ * to use this feature with modules that affect Drupal majorly such as
+ * the language or field module.
+ */
+$settings['config_exclude_modules'] = [
+  'stage_file_proxy',
+  'devel_entity_updates',
+  'mysql57',
+];
+
+/**
  * Fast 404 pages:
  *
  * Drupal can generate fully themed 404 pages. However, some of these responses
