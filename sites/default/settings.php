7331a03144f582883305684efa439868a8553814
Ruslan Piskarov <ruslan.piskarev@kwallcompany.com>
2026-07-13 15:40:48 +0300

    Add contact module.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -801,6 +801,8 @@ $settings['state_cache'] = TRUE;
 // Simple XML Sitemap.
 $settings['simple_sitemap_engines.index_now.key'] = '5c9a7dfc-bbb7-4426-899d-a1611dc93426';
 
+$settings['enable_html5_validation'] = FALSE;
+
 // Automatically generated include for settings managed by ddev.
 $ddev_settings = __DIR__ . '/settings.ddev.php';
 if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = 'b4818b01b3a40467';
+  public const string VERSIONS_HASH = '25e6ed561f788d58';
 
 }
